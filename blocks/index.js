6d85a8a62d2ceefa7bba12c24b8b7f7ab59910e7
Riddhi <riddhinimeshshah10@gmail.com>
2024-03-28 16:13:20 +0530

    .

--- blocks/index.js
+++ blocks/index.js
@@ -1,4 +1,7 @@
 function update(amount){
     const display=document.getElementById("entry");
     display.value=parseInt(display.value)+amount;
+
+    const click_sound=document.getElementById("sound");
+    click_sound.play();
 }
